8ad2a06c94961d7ed02581f682646135e550f40e
gencery <genceryigiter@gmail.com>
2023-03-02 17:26:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import SearchBar from './SearchBar';
 
 export default function App() {
 
-  const [SearchBarResult, useSearchBarResult] = useState("");
+  const [SearchBarResult, setSearchBarResult] = useState("");
 
   return (
-    <div className='App'>
 
+    <div className='App'>
       <Logo logoUrl={"https://intranet.izbb.net/Assets/img/anasayfaProjelerSlider/dijitalKitap2.png"} />
 
       <span className='container'>
         <span>{(SearchBarResult === "") ? "-" : SearchBarResult}</span>
       </span>
 
-      <SearchBar handleSearch={useSearchBarResult} />
+      <SearchBar handleSearch={setSearchBarResult} />
 
     </div>
+
   );
 }
