38a37dcc6908a7b5564d4651cbfb869c987174c9
gencery <genceryigiter@gmail.com>
2023-03-10 14:54:24 +0300

    .

--- src/Test.js
+++ src/Test.js
@@ -1,27 +1,21 @@
 import React, { useState, useRef, useEffect } from "react";
 
 export default function Test() {
-  const [state, setState] = useState(0);
+  const [num, setNum] = useState(0);
 
   const renderCount = useRef(0);
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
-  });
+  }, [num]);
 
   return (
     <>
       <p>Render no: {renderCount.current}</p>
-      <p>State: {state}</p>
+      <p>State: {num}</p>
 
       <button onClick={() => {
-        // setState(state + 1)
-        // setState(state + 1)
-        // setState(state + 1)
-
-        setState(state + 1)
-        setState(state => state + 1)
-        setState(42)
+        setNum(num + 1)
 
       }}>Click to increment</button>
 
